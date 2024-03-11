a69dd7b36052c2ef72d6fb9f15820ac9c851d84a
harshita <harshita@logicrays.com>
2024-03-11 09:44:51 +0530

    third commit

--- usecallback-demo/src/App.jsx
+++ usecallback-demo/src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import { useCallback, useEffect, useRef, useState } from 'react';
 function App() {
-  const [count, setCount] = useState(0)
+  let [password,setPassword]=useState('')
+  let [length,setLength]=useState(8)
+  let [numberAllowed,setNumberAllowed]=useState(false)
+  let [charAllowed,setCharAllowed]=useState(false)
+  let passwordRef=useRef()
 
+  // let passwordGenerator=()=>{
+  //   let pass=''
+  //   let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  //   if(numberAllowed) str+="1234567890"
+  //   if(charAllowed) str+="!@#$%^&*()-+=."
+  //   for(let i=1;i<=length;i++){
+  //       pass+= str.charAt(Math.floor(Math.random()*str.length))
+  //   }
+  //   setPassword(pass)
+  // }
+
+  let passwordGenerator=useCallback(()=>{
+    let pass=''
+  let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  if(numberAllowed) str+="1234567890"
+  if(charAllowed) str+="!@#$%^&*()-+=."
+  for(let i=1;i<=length;i++){
+      pass+= str.charAt(Math.floor(Math.random()*str.length))
+  }
+  setPassword(pass)
+},[length,numberAllowed,charAllowed])
+
+let copyPassword=()=>{
+  passwordRef.current?.select()
+  passwordRef.current?.setSelectionRange(0,5)
+}
+
+  useEffect(()=>{
+    passwordGenerator()
+  },[length,numberAllowed,charAllowed])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+    <InputGroup className="mb-3">
+        <Form.Control type="text" value={password} ref={passwordRef}/>
+        <Button variant="primary" id="button-addon2" onClick={copyPassword}>
+          copy
+        </Button>
+      </InputGroup>
+      <Row>
+        <Col> <input type="range" min={6} max={100} value={length} onChange={(e)=>setLength(e.target.value)}/> length({length})  
+       <input type="checkbox" defaultChecked={numberAllowed} 
+        onChange={()=>setNumberAllowed(!numberAllowed)}></input> Numbers
+        <input type="checkbox" defaultChecked={charAllowed}
+        onChange={()=>setCharAllowed(prev=>!prev)}></input> Character</Col>
+      </Row>
+      
+   </>
   )
 }
 
